Fixed numerical part

oblig1_opp2.py
@@ -3,12 +3,15 @@ import matplotlib.pyplot as plt
 from scipy import constants,stats
 from scipy.integrate import ode
 
-def H(omega_m,omega_l,t,H0):
+def H(omega_m,omega_l,t,H0,a):
+
+    return H0*np.sqrt(omega_m*a**(-3)+omega_l*a)
+
     if omega_m == 1:
         return 2/(3*t)
     else:
         return H0*np.sqrt(omega_l)*np.cosh(3./2*np.sqrt(omega_l)*H0*t)\
-                            *(np.sinh(3./2*np.sqrt(omega_l)*H0*t))**(1/3)
+                            *(np.sinh(3./2*np.sqrt(omega_l)*H0*t))**(-1)
 
 def t(omega_m,omega_l,a,H0):
     if omega_m == 1:
@@ -16,7 +19,8 @@ def t(omega_m,omega_l,a,H0):
     else:
         return 2./3*1/(np.sqrt(omega_l)*H0)*np.arcsinh(((omega_m/omega_l)**(-1./3)*a)**(3./2))
 
-def a_dot(omega_m,omega_l,t,H0):
+def a_dot(omega_m,omega_l,t,H0,a):
+    return H0*np.sqrt(omega_m*a**(-1)+omega_l*a**(2))
     if omega_m == 1:
         return (3./2*H0)**(2./3)*t**(-1./3)*2./3
     else:
@@ -32,9 +36,10 @@ def a(omega_m,omega_l,t,H0):
 
 def dxdt(omega_m,omega_l,a,t,H0,delta,x):
     G = constants.G
-    H_ = H(omega_m,omega_l,t,H0)
+    H_ = H(omega_m,omega_l,t,H0,a)
+    return delta*3/2.*H0**2*a**(-3)- 2*H_*x
     rho0 = 3*H0**2/(8*np.pi*G)
-    return delta*4*np.pi*rho0*a**(-3) - 2*H_*x
+    return delta*4*np.pi*rho0*a**(-3)*G - 2*H_*x
 
 def ddeltadt(x):
     return x
@@ -49,33 +54,36 @@ def x_step(t,delta,args):
 
 def solve(omega_m,omega_l,delta0,a0,steps,H0):
     t_array = np.linspace(t(omega_m,omega_l,a0,H0),t(omega_m,omega_l,1,H0),steps)
-    #print(t_array)
-    #exit()
     a_array = a(omega_m,omega_l,t_array,H0)
     delta_array = np.zeros_like(a_array)
     delta_array[0] = delta0
     x_array = np.zeros_like(a_array)
-    x_array[0] = a_dot(omega_m,omega_l,t_array[0],H0)
+    x_array[0] = a_dot(omega_m,omega_l,t_array[0],H0,a_array[0])
 
     dt = t_array[1]-t_array[0]
 
 
+
+
+    for i in range(steps-1):
+
+        dx = dxdt(omega_m,omega_l,a_array[i],t_array[i],H0,delta_array[i],x_array[i])*dt
+        x_array[i+1] = x_array[i] + dx
+        ddelta = x_array[i]*dt
+        delta_array[i+1] = delta_array[i] + ddelta
+        if delta_array[i+1] != delta_array[i+1]:
+            break
+
+
+
+    """
     r_delta = ode(delta_step).set_integrator('lsoda', method='bdf')
     r_delta.set_initial_value(delta_array[0],t_array[0]).set_f_params([x_array[0]])
 
     r_x = ode(x_step).set_integrator('lsoda', method='bdf')
     r_x.set_initial_value(x_array[0],t_array[0]).set_f_params([omega_m,omega_l,a_array[0],H0,x_array[0]])
-    """
-    for i in range(steps-1):
-
-        dx = dxdt(omega_m,omega_l,a_array[i],t_array[i],H0,delta_array[i],x_array[i])*dt
-        x_array[i+1] = x_array[i] + dx
-        ddelta = x_array[i+1]*dt
-        delta_array[i+1] = delta_array[i] + ddelta
-        if delta_array[i+1] != delta_array[i+1]:
-            break
-    """
     i = 0
+
     while r_delta.successful() and r_x.successful() and r_delta.t <= t_array[-1]:
         r_x.set_f_params([omega_m,omega_l,a_array[i],H0,x_array[i]])
 
@@ -84,7 +92,7 @@ def solve(omega_m,omega_l,delta0,a0,steps,H0):
         r_delta.set_f_params([x_array[i+1]])
         delta_array[i+1] = r_delta.integrate(r_delta.t + dt)[0]
 
-
+    """
     return delta_array,a_array
 
 def find_power(delta,a):
@@ -92,7 +100,7 @@ def find_power(delta,a):
 
 if __name__ == '__main__':
     omega_m = 1.
-    omega_l = 0.
+    omega_l = .0
     H0 = 2e-18
     delta0 = 1e-3
     a0 = 1e-3