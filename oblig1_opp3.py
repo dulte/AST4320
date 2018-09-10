34572641755fc2fb1ebdac9346d036e09e1e55ad
Daniel <daniel.heinesen@sf-nett.no>
2018-09-10 16:25:50 +0200

    Starts with 3b

--- oblig1_opp3.py
+++ oblig1_opp3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import constants
 try:
     import seaborn as sns
     sns.set_style("darkgrid")
@@ -12,7 +13,40 @@ def T_photon(a):
 def T_gas(a):
     return 4e-3*1./a**(2)
 
+def sound_speed(z,type_):
+    c = constants.c
+    k = constants.k
+    mp = constants.m_p
+    mu = 0.63
+    if type_=="photons":
+        return c/np.sqrt(3)
+    else:
+        return np.sqrt(T_gas(1./(1+z))*k/(mu*mp))
+
+def density(z):
+    G = constants.G
+    a = 1./(z+1)
+    H = 2e-18*np.sqrt(0.3*a**(-3)+0.7)
+    rho = 3*H**2/(8*np.pi*G)
+    return rho
+
+
+
+def jeans_length(z,type_):
+    G = constants.G
+    c = np.zeros_like(z)
+    c = np.where(z>1100,sound_speed(z,"photons"),sound_speed(z,type_))
+    """
+    if z>=1100:
+        c = sound_speed(z,"photons")
+    else:
+        c = sound_speed(z,type_)
+    """
+    return c*np.sqrt(np.pi/(G*density(z)))
+
 if __name__ == '__main__':
+    #a)
+    """
     a = np.linspace(1e-4,1,1000)
     plt.loglog(a,T_photon(a),label="Photons")
     plt.loglog(a,T_gas(a),label="Gas")
@@ -20,4 +54,14 @@ if __name__ == '__main__':
     plt.xlabel("a",fontsize=15)
     plt.ylabel("T [K]",fontsize=15)
     plt.legend(loc="best",fontsize=15)
+    plt.show()
+    """
+    #b)
+    z = np.linspace(2000,0,10000)
+    jeans_length_photons = jeans_length(z,"photons")
+    jeans_length_gas = jeans_length(z,"gas")
+    plt.loglog(z,jeans_length_photons,label="Photons")
+    plt.loglog(z,jeans_length_gas,label="Gas")
+    plt.legend()
+    plt.xlim(z[0],z[-1])
     plt.show()
