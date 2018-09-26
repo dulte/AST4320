d6761cb1907d8852cd9213f4e946f4f481cd9c2e
Daniel <daniel.heinesen@sf-nett.no>
2018-09-26 18:05:26 +0200

    Begins oblig 2

---
+++ Oblig 2/ex1.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def W(k,R):
+    return np.where(k == 0,2*R,2*np.sin(k*R)/k)
+
+def find_FWHM(k,W):
+    W_max = np.max(W)
+    HM = W_max/2.
+    FWHM = k[np.argmin(np.abs(W-HM))]
+    return [[FWHM,-FWHM],HM]
+
+
+k = np.linspace(-8*np.pi,8*np.pi,1000)
+R = 1
+
+W_array = W(k,R)
+
+FWHM = find_FWHM(k,W_array)
+length = np.max(FWHM[0]) - np.min(FWHM[0])
+
+
+
+print("FWHM = %g"%(length))
+plt.plot(k,W_array,label=r"$\tilde{W}(k)$")
+plt.annotate(s='', xy=(np.max(FWHM[0]),FWHM[1]), xytext=(np.min(FWHM[0]),FWHM[1]), arrowprops=dict(arrowstyle='<->'),label="FWHM")
+plt.legend()
+plt.show()
